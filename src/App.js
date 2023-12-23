90c2adb81aabb2b17345d6350243d235676fde4c
sidharth980 <kidsachu@gmail.com>
2023-12-23 14:14:40 +0530

    Checkpoint 4 - added delete and subhead

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import Header from "./components/Header/Header";
 import ToDoList from "./components/ToDoList/ToDoList";
+import { useState } from "react";
 import "./styles.css";
+import Subhead from "./Subheader";
 
 export default function App() {
-  const todoList = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"]
+  let localToDoList = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"]
+  let [subheadText,setsubheadText] = useState();
+  const [todoList,settodoList] = useState(localToDoList);
   
+  function deleteList(){
+    localToDoList = [null];
+    settodoList(localToDoList);
+    setsubheadText("Nothing to do buddy.Sleep!")
+  }
+
+
   return (
     <div className="Application">
       <Header headerText={"ToDo app"}/>
+      <Subhead text={subheadText}/>
       <div className = "todo">
         <ul>
-      {todoList.map((itm,index) => (
+           {todoList.map((itm,index) => (
         <ToDoList key={"list"+index} text={itm} />
       ))}
       </ul>
+      <button className="delete-button" onClick={deleteList} >Delete</button>
       </div>
       
     </div>
---
+++ src/Subheader.js
@@ -0,0 +1,5 @@
+export default function Subhead({text}){
+    return (
+        <h3 className="subhead">{text}</h3>
+    )
+}
