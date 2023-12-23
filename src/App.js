ac79ce0f081fa2677eb619f1cca4bbd40c53d9d4
sidharth980 <kidsachu@gmail.com>
2023-12-23 13:48:25 +0530

    checkpoint 3 - added todo list upddated css and fixed key errro

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import ToDoList from "./components/ToDoList/ToDoList";
 import "./styles.css";
 
 export default function App() {
+  const todoList = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", "Sleep for 2 hours", "Take a shower"]
+  
   return (
     <div className="Application">
       <Header headerText={"ToDo app"}/>
-      <ToDoList text={"Nothing to do buddy. Sleep!!"}/>
+      <div className = "todo">
+        <ul>
+      {todoList.map((itm,index) => (
+        <ToDoList key={"list"+index} text={itm} />
+      ))}
+      </ul>
+      </div>
+      
     </div>
   );
 }
